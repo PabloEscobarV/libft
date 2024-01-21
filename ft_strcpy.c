54b1be1335957609b2467eff32d0f248a0d1fe6c
Pablo Escobar <sataniv.rider@gmail.com>
2024-01-21 20:27:47 +0100

    changed strcpy, strjoinfree

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,15 +6,17 @@
 /*   By: blackrider <blackrider@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 17:20:07 by blackrider        #+#    #+#             */
-/*   Updated: 2024/01/21 17:29:25 by blackrider       ###   ########.fr       */
+/*   Updated: 2024/01/21 18:59:06 by blackrider       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 char	*ft_strcpy(char *dest, char const *src)
 {
     if (!src)
-        return (dest);
-	while (*src != 0)
+		return (dest);
+	while (*src != '\0')
 	{
 		*dest = *src;
 		++dest;
