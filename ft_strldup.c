ce2d675e94ba19d40d64dbbac98e2384f65f3ca8
Pablo Escobar <sataniv.rider@gmail.com>
2024-08-01 20:33:03 +0200

    update ft_strldup.c

--- ft_strldup.c
+++ ft_strldup.c
@@ -6,7 +6,7 @@
 /*   By: Pablo Escobar <sataniv.rider@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 18:09:11 by Pablo Escob       #+#    #+#             */
-/*   Updated: 2024/07/22 18:17:43 by Pablo Escob      ###   ########.fr       */
+/*   Updated: 2024/08/01 20:18:53 by Pablo Escob      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strldup(const char *src, int n)
 	if (!tmp)
 		return (NULL);
 	tmp[n] = '\0';
-	while (n || src[--n])
+	while (--n)
 		tmp[n] = src[n];
 	return (tmp);
 }
