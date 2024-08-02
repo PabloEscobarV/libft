c14673cc5070aa0ca2598dae540a91c30d48dde7
Pablo Escobar <sataniv.rider@gmail.com>
2024-08-02 14:55:18 +0200

    redigne ft_strncpy to len

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,23 +6,23 @@
 /*   By: Pablo Escobar <sataniv.rider@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 12:30:24 by polenyc           #+#    #+#             */
-/*   Updated: 2024/08/02 14:51:08 by Pablo Escob      ###   ########.fr       */
+/*   Updated: 2024/08/02 14:54:37 by Pablo Escob      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strncpy(char *dest, const char *src, size_t size)
+char	*ft_strncpy(char *dest, const char *src, size_t len)
 {
 	size_t	i;
 
 	if (!dest)
 		return (NULL);
-	if (size <= 0 || !src)
+	if (len <= 0 || !src)
 		return (dest);
-	dest[--size] = '\0';
+	dest[len] = '\0';
 	i = -1;
-	while (++i < size)
+	while (++i < len)
 		dest[i] = src[i];
-	return (dest + size);
+	return (dest + len);
 }
