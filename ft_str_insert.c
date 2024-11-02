719ffe1e352e8f446d095d48dc36cc9d3ba49c1e
Pablo Escobar <sataniv.rider@gmail.com>
2024-11-02 23:20:36 +0100

    libft: Terminal_3: fixed ft_str_insert

--- ft_str_insert.c
+++ ft_str_insert.c
@@ -6,7 +6,7 @@
 /*   By: Pablo Escobar <sataniv.rider@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 17:17:58 by Pablo Escob       #+#    #+#             */
-/*   Updated: 2024/11/02 23:09:24 by Pablo Escob      ###   ########.fr       */
+/*   Updated: 2024/11/02 23:18:36 by Pablo Escob      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,8 @@ char	*ft_str_insert_ch_ind(const char *str, const char *str_to, char insrt)
 		rear = front;
 		while (str[rear] && !ft_isspace(str[rear]))
 			++rear;
+		if (!str[front])
+			return (NULL);
 	}
 	return (ft_str_insert(str, str_to, front, rear));
 }
